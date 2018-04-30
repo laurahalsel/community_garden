f54a1c3704b4183dc2dac8a56d00a2254c883989
Halsel <lhalsel@spartan.northampton.edu>
2018-04-30 19:04:35 -0400

    test update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);   
         $this->call(SystemsTableSeeder::class);
         $this->call(PlantsTableSeeder::class);
         $this->call(NotesTableSeeder::class);
